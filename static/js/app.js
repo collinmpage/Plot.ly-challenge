5d04f7a9227f1ee9f1f686f28773eb47711a6bb5
Collin Page <collinmpage@gmail.com>
2021-04-14 22:45:10 -0500

    finishing setting up assignment and reading in the json file

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,7 @@
 // const data = './samples.json'
 
 // loading in the data
-d3.json('./samples.json')
-    .then(function(data) {
+d3.json('../samples.json').then(function(data) {
     //read the data
     console.log(data);
     });
