a2e3f2d0cb6d6140863f68b30aec8998d26af9a7
Collin Page <collinmpage@gmail.com>
2021-04-14 23:01:59 -0500

    updating app.js to read json file correctly

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,10 @@
 // const data = './samples.json'
 
 // loading in the data
-d3.json('../samples.json').then(function(data) {
+d3.json('/../../samples.json').then((data) => {
     //read the data
     console.log(data);
     });
+
+const dataPromise = d3.json('/../../samples.json');
+console.log("Data Promise: ", dataPromise);
